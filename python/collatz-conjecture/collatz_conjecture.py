d12914064b35d08db45f1a24e313ec803c001398
Zach <zjgass@gmail.com>
2023-11-08 21:09:37 -0500

    Completed collatz_conjecture. Learned that recursion is slower in python than just if/else.

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,18 @@
 def steps(number):
-    pass
+    """
+
+    :param number: int - the initial number for which to count steps in the
+    Collatz Conjecture.
+    :return: int - how many steps were required to get to 1.
+    """
+
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+    elif number == 1:
+        return 0
+    elif number % 2 == 0:
+        number = number / 2
+    else:
+        number = 3 * number + 1
+
+    return 1 + steps(number)
