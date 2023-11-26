93f92dabb9fbe701cf086e2d15509d1219d0b1ab
Zach <zjgass@gmail.com>
2023-11-25 21:23:58 -0500

    Completed triangle.

---
+++ python/triangle/triangle.py
@@ -0,0 +1,57 @@
+def equilateral(sides):
+    """
+
+    :param sides: list - the sides to test
+    :return: bool - is the triangle equilateral
+    """
+    
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    return (is_triangle(sides)
+            and a == b and b == c)
+
+
+def isosceles(sides):
+    """
+
+    :param sides: list - the sides to test
+    :return: bool - is the triangle isosceles
+    """
+
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    return (is_triangle(sides)
+            and (a == b or b == c or a == c))
+
+
+def scalene(sides):
+    """
+
+    :param sides: list - the sides to test
+    :return: bool is the triangle scalene
+    """
+
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    return (is_triangle(sides)
+            and a != b and b != c and a != c)
+
+def is_triangle(sides):
+    """
+
+    :pa;am sides: int - the list of sides to test
+    :return: bool - is a valid triangle
+    """
+
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    return (a > 0 and b > 0 and c > 0
+            and a + b >= c and b + c >= a and a + c >= b)
